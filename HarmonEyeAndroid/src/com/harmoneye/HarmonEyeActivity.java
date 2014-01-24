5e995e201020c79d09231f1f9d097ffbc3f285cc
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2014-01-24 18:13:38 +0100

    Let the screen enabled while HarmonEye is running.
    It previously dimmed after some time.

--- HarmonEyeAndroid/src/com/harmoneye/HarmonEyeActivity.java
+++ HarmonEyeAndroid/src/com/harmoneye/HarmonEyeActivity.java
@@ -35,6 +35,7 @@ public class HarmonEyeActivity extends Activity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 			WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
 		glView = new MyGLSurfaceView(this);
 		visualizer = new OpenGlVisualizer(glView);
